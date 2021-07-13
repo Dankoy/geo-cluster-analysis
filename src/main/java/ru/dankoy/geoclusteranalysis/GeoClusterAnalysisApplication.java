07560df1781ff9026e921632c576cc8ea976bcc4
Evgenii Zelenin <ezelenin@dtln.ru>
2021-07-13 18:11:26 +0300

    init: initial commit

---
+++ src/main/java/ru/dankoy/geoclusteranalysis/GeoClusterAnalysisApplication.java
@@ -0,0 +1,13 @@
+package ru.dankoy.geoclusteranalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoClusterAnalysisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GeoClusterAnalysisApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/dankoy/geoclusteranalysis/GeoClusterAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dankoy.geoclusteranalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoClusterAnalysisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
